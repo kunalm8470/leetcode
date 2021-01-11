2f9f128502b9c611343637c919c680f788ff496d
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-11 17:21:50 +0530

    Two sum problem

---
+++ arrays/two_sum/two_sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace two_sum
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            if (nums == default(int[]) || nums.Length == 0)
+            {
+                return null;
+            }
+
+            Dictionary<int, int> lookup = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                
+                if (lookup.ContainsKey(diff))
+                {
+                    return new int[] { i, lookup[diff] };
+                }
+
+                lookup.Add(nums[i], i);
+            }
+
+            return new int[2];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] res = TwoSum(nums, target);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write(res[i] + " ");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
