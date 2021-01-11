f544f55d708717acafa5bc65c3c21eab6337e43e
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-11 19:41:04 +0530

    Added remove element

---
+++ arrays/remove_element/remove_element/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace remove_element
+{
+    class Program
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums == default(int[]) || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int idx = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[idx++] = nums[i];
+                }
+            }
+
+            return idx;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] inp = { 3, 2, 2, 3 };
+            int val = 3;
+            int res = RemoveElement(inp, val);
+            Console.WriteLine(res);
+        }
+    }
+}
