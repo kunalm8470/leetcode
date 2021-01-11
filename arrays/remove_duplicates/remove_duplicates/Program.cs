7436c0ccd51dc704f77b8ac6b471c1deb0581644
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-11 18:07:43 +0530

    Remove duplicate added

---
+++ arrays/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums == default(int[]) || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            return i + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 2 };
+            int res = RemoveDuplicates(nums);
+            Console.WriteLine(res);
+        }
+    }
+}
