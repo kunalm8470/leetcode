223811389733d28cb89d302fea8160475feb1da3
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-12 21:01:09 +0530

    Added string multiplication

---
+++ arrays/string_multiply/string_multiply/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace string_multiply
+{
+    class Program
+    {
+        public static string Multiply(string num1, string num2)
+        {
+            BigInteger b1 = BigInteger.Parse(num1);
+            BigInteger b2 = BigInteger.Parse(num2);
+            return (b1 * b2).ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply("2", "3"));
+        }
+    }
+}
