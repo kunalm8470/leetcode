bfbbff03367ca3c30a1071872bcec01a747159a4
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-16 11:12:15 +0530

    Added majority element in array

---
+++ arrays/majority_element_in_array/majority_element_in_array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace majority_element_in_array
+{
+    class Program
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            
+            int threshold = Convert.ToInt32(Math.Floor(Convert.ToDouble(nums.Length / 2)));
+            Dictionary<int, int> frequencyLookup = new Dictionary<int, int>();
+
+            foreach (int item in nums)
+            {
+                if (!frequencyLookup.ContainsKey(item))
+                {
+                    frequencyLookup[item] = 1;
+                }
+                else
+                {
+                    frequencyLookup[item] += 1;
+                }
+            }
+
+            (int key, int value) = frequencyLookup.FirstOrDefault((kvp) => kvp.Value > threshold);
+            return key;
+        }
+
+        static void Main(string[] args)
+        {
+            int res1 = MajorityElement(new int[] { 3, 2, 3 });
+            int res2 = MajorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 });
+        }
+    }
+}
