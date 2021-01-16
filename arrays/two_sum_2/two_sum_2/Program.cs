519c1abfe28ac0ce3311b83863c195c7c7821db7
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-16 10:59:01 +0530

    Added variant of two sum problem

---
+++ arrays/two_sum_2/two_sum_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace two_sum_2
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2];
+            if (numbers == default(int[]) || numbers.Length == 0)
+            {
+                return result;
+            }
+
+            int leftIndex = 0;
+            int rightIndex = numbers.Length - 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[leftIndex] + numbers[rightIndex];
+                if (current < target)
+                {
+                    leftIndex += 1;
+                }
+                else if (current > target)
+                {
+                    rightIndex -= 1;
+                }
+                else
+                {
+                    result[0] = leftIndex + 1;
+                    result[1] = rightIndex + 1;
+                    return result;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            // Pre-condition is that array will be sorted
+            int[] arr = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] res = TwoSum(arr, target);
+        }
+    }
+}
