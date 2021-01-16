872c1c53a989a3b7022691cbef0650778330a2f3
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-16 20:00:39 +0530

    Added move zeroes

---
+++ arrays/move_zeroes/move_zeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace move_zeroes
+{
+    class Program
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int leftIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[leftIndex];
+                    nums[leftIndex] = temp;
+
+                    leftIndex += 1;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 0, 0 };
+            MoveZeroes(nums);
+        }
+    }
+}
