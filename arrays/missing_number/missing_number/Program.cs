80eb0355e6f8d36fbcf0b4787cb7ab6cf55990df
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-16 19:35:30 +0530

    Added missing numbers

---
+++ arrays/missing_number/missing_number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace missing_number
+{
+    class Program
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            int firstNSum = Convert.ToInt32((nums.Length * (nums.Length + 1)) / 2);
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return firstNSum - sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int res1 = MissingNumber(new int[] { 3, 0, 1 });
+            int res2 = MissingNumber(new int[] { 0, 1 });
+            int res3 = MissingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 });
+            int res4 = MissingNumber(new int[] { 0 });
+        }
+    }
+}
