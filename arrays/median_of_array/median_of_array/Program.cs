06e21b371f4426303a1a63ba28c61928af8c88c3
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-18 15:35:18 +0530

    Added median of arrays

---
+++ arrays/median_of_array/median_of_array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace median_of_array
+{
+    class Program
+    {
+		public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+		{
+			int m = nums1.Length, n = nums2.Length;
+			int[] res = new int[m + n];
+
+			int i = 0, j = 0, k = 0;
+			while (i < m && j < n)
+			{
+				if (nums1[i] < nums2[j])
+				{
+					res[k++] = nums1[i++];
+				}
+
+				else
+				{
+					res[k++] = nums2[j++];
+				}
+			}
+
+			while (i < m)
+			{
+				res[k++] = nums1[i++];
+			}
+
+			while (j < n)
+			{
+				res[k++] = nums2[j++];
+			}
+
+			double median = 0.0;
+			if (res.Length % 2 == 0)
+			{
+				int prev = (res.Length / 2) - 1;
+				int next = res.Length / 2;
+				median = (res[prev] + res[next]) / 2.0;
+			}
+			else
+			{
+				median = res[res.Length / 2];
+			}
+
+			return median;
+		}
+
+		static void Main(string[] args)
+        {
+			double res1 = FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+		}
+    }
+}
