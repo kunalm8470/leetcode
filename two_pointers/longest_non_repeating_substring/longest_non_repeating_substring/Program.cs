46b552b2a8bcd046775cc92b5db18d1494a0dc19
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-19 12:42:48 +0530

    Added longest non repeating substring

---
+++ two_pointers/longest_non_repeating_substring/longest_non_repeating_substring/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace longest_non_repeating_substring
+{
+    class Program
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int i = 0, j = 0;
+            int max = 0;
+            HashSet<char> hs = new HashSet<char>();
+
+            while (j < s.Length)
+            {
+                if (!hs.Contains(s[j]))
+                {
+                    hs.Add(s[j]);
+                    j += 1;
+                    max = Math.Max(max, hs.Count);
+                }
+                else
+                {
+                    hs.Remove(s[i]);
+                    i += 1;
+                }
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            int res1 = LengthOfLongestSubstring("abcabcbb");
+            int res2 = LengthOfLongestSubstring("bbbbb");
+            int res3 = LengthOfLongestSubstring("pwwkew");
+            int res4 = LengthOfLongestSubstring("");
+        }
+    }
+}
