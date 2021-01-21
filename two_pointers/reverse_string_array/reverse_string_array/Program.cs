bdd57cfc415cb8d1de137c09a019737eac2e00c4
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-01-21 18:04:07 +0530

    Added reverse string array using two pointers

---
+++ two_pointers/reverse_string_array/reverse_string_array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace reverse_string_array
+{
+    class Program
+    {
+        public static void ReverseString(string[] s)
+        {
+            int i = 0, length = s.Length;
+            while (i < (length / 2))
+            {
+                string temp = s[i];
+                s[i] = s[length - 1 - i];
+                s[length - 1 - i] = temp;
+                i += 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] inp1 = { "h","e","l","l","o" };
+            ReverseString(inp1);
+        }
+    }
+}
