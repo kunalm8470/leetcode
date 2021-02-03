7b4029254e9c36af0043816004a8024fae5b54fa
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-02-03 18:48:27 +0530

    Added Isomorphic string

---
+++ hash_table/isomorphic_strings/isomorphic_strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace isomorphic_strings
+{
+    class Program
+    {
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            int[] lookupS = new int[256];
+            int[] lookupT = new int[256];
+            int counter = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Not matching early-exit
+                if (lookupS[s[i]] != lookupT[t[i]])
+                {
+                    return false;
+                }
+
+                // First time huh ?
+                if (lookupS[s[i]] == 0)
+                {
+                    lookupS[s[i]] = counter;
+                    lookupT[t[i]] = counter;
+                    counter += 1;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            bool res1 = IsIsomorphic("egg", "add");
+            bool res2 = IsIsomorphic("foo", "bar");
+            bool res3 = IsIsomorphic("paper", "title");
+            bool res4 = IsIsomorphic("badc", "baba");
+        }
+    }
+}
