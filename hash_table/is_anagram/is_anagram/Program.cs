571d43ac6e3865ade3164d95f76ae1c4efca40b5
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-02-18 20:04:23 +0530

    Added is_anagram

---
+++ hash_table/is_anagram/is_anagram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace is_anagram
+{
+    class Program
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> lookup = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!lookup.ContainsKey(s[i]))
+                {
+                    lookup[s[i]] = 1;
+                }
+                else
+                {
+                    lookup[s[i]] += 1;
+                }
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!lookup.ContainsKey(t[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    lookup[t[i]] -= 1;
+                }
+            }
+
+            return lookup.Values.All(x => x == 0);
+        }
+
+        static void Main(string[] args)
+        {
+            bool res1 = IsAnagram("anagram", "nagaram");
+            bool res2 = IsAnagram("rat", "car");
+        }
+    }
+}
