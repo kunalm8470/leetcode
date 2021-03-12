814793b01425957db27763ba59bfd89ef3207a2a
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-03-12 19:53:59 +0530

    Added validate parenthesis

---
+++ strings/valid_parenthesis/valid_parenthesis/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace valid_parenthesis
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stk = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentToken = s[i];
+                bool isOpening = (currentToken == '(' || currentToken == '[' || currentToken == '{');
+                bool isClosing = (currentToken == ')' || currentToken == ']' || currentToken == '}');
+
+                // Edge case for closing paren
+                if (isClosing && stk.Count == 0)
+                {
+                    return false;
+                }
+
+                if (isOpening)
+                {
+                    stk.Push(currentToken);
+                }
+
+                else if (isClosing)
+                {
+                    char top = stk.Peek();
+                    if (currentToken == ')' && top != '(')
+                    {
+                        return false;
+                    }
+
+                    if (currentToken == ']' && top != '[')
+                    {
+                        return false;
+                    }
+
+                    if (currentToken == '}' && top != '{')
+                    {
+                        return false;
+                    }
+
+                    stk.Pop();
+                }
+            }
+
+            return stk.Count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            bool res1 = IsValid("(]");
+            bool res2 = IsValid("]");
+        }
+    }
+}
