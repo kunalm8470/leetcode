805886d6c120c899f684598d9c401759aaf82cef
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-03-14 00:09:23 +0530

    Added roman number to decimal

---
+++ strings/roman_to_decimal/roman_to_decimal/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace roman_to_decimal
+{
+    class Program
+    {
+		/// <summary>
+		/// <para>https://leetcode.com/problems/roman-to-integer</para>
+		/// <para>https://www.mathsisfun.com/roman-numerals.html</para>
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public static int RomanToInt(string s)
+		{
+			Dictionary<char, int> digitLookup = new Dictionary<char, int>
+			{
+				['I'] = 1,
+				['V'] = 5,
+				['X'] = 10,
+				['L'] = 50,
+				['C'] = 100,
+				['D'] = 500,
+				['M'] = 1000
+			};
+
+			List<int> digits = new List<int>();
+			for (int i = 0; i < s.Length; i++)
+			{
+				digits.Add(digitLookup[s[i]]);
+			}
+
+			int sum = 0;
+			for (int i = 0; i < digits.Count; i++)
+			{
+				if (i == digits.Count - 1)
+                {
+					sum += digits[i];
+				}
+
+				else
+                {
+					if (digits[i] < digits[i + 1])
+					{
+						sum -= digits[i];
+					}
+
+					else if (digits[i] >= digits[i + 1])
+					{
+						sum += digits[i];
+					}
+				}
+
+			}
+
+			return sum;
+		}
+
+		static void Main(string[] args)
+        {
+			int res1 = RomanToInt("III");
+			int res2 = RomanToInt("IV");
+			int res3 = RomanToInt("IX");
+			int res4 = RomanToInt("LVIII");
+			int res5 = RomanToInt("MCMXCIV");
+			int res6 = RomanToInt("DCXXI");
+			int res7 = RomanToInt("CXLIV");
+		}
+    }
+}
