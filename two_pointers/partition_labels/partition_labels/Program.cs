3458a05b1ab4aceca0e3bc4d17c2228db4364efd
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-03-14 23:46:12 +0530

    Added partition labels

---
+++ two_pointers/partition_labels/partition_labels/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace partition_labels
+{
+    class Program
+    {
+        public static IList<int> PartitionLabels(string S)
+        {
+            int[] lastIndexArr = new int[26];
+            for (int i = 0; i < S.Length; i++)
+            {
+                lastIndexArr[S[i] - 'a'] = i;
+            }
+
+            IList<int> result = new List<int>();
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                end = Math.Max(end, lastIndexArr[S[i] - 'a']);
+                if (i == end)
+                {
+                    result.Add(end - start + 1);
+                    start = end + 1;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            IList<int> res1 = PartitionLabels("ababcbacadefegdehijhklij");
+        }
+    }
+}
