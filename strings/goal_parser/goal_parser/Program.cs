ad961a52a9ac04ac3ae62d250d4c5ab71399039e
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-03-15 21:40:44 +0530

    Added goal parser

---
+++ strings/goal_parser/goal_parser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace goal_parser
+{
+    class Program
+    {
+        public static string Interpret(string command)
+		{
+			StringBuilder builder = new StringBuilder();
+
+			Stack<char> stk = new Stack<char>();
+			for (int i = 0; i < command.Length; i++)
+			{
+				if (command[i] == '(' && command[i + 1] == ')')
+				{
+					stk.Push('(');
+				}
+				else if (
+					(command[i] == '(' && command[i + 1] != ')')
+					|| (command[i] == ')' && command[i - 1] != '(')
+				)
+                {
+					continue;
+                }
+				else if (stk.Count != 0 && command[i] == ')' && stk.Pop() == '(')
+				{
+					builder.Append('o');
+				}
+				else
+				{
+					builder.Append(command[i]);
+				}
+			}
+
+			return builder.ToString();
+		}
+
+		static void Main(string[] args)
+        {
+            string res1 = Interpret("G()(al)");
+            string res2 = Interpret("G()()()()(al)");
+			string res3 = Interpret("(al)G(al)()()G");
+        }
+    }
+}
