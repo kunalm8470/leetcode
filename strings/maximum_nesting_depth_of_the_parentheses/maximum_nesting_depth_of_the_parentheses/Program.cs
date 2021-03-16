b97ff156d0ef9d46a583ea5d286ed424b850512e
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-03-16 22:30:13 +0530

    Added maximum nesting depth of the parenthesis

---
+++ strings/maximum_nesting_depth_of_the_parentheses/maximum_nesting_depth_of_the_parentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace maximum_nesting_depth_of_the_parentheses
+{
+    class Program
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int MaxDepth(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s == "1") return 0;
+
+            int depth = 0;
+            int openParens = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(') openParens++;
+                else if (s[i] == ')') openParens--;
+                depth = Math.Max(depth, openParens);
+            }
+
+            return depth;
+        }
+
+        static void Main(string[] args)
+        {
+            int res1 = MaxDepth("()()");
+            int res2 = MaxDepth("()(()())");
+            int res3 = MaxDepth("");
+            int res4 = MaxDepth("1");
+            int res5 = MaxDepth("(1+(2*3)+((8)/4))+1");
+            int res6 = MaxDepth("(1)+((2))+(((3)))");
+            int res7 = MaxDepth("1+(2*3)/(2-1)");
+        }
+    }
+}
